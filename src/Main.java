a077b2738fc08ddafb15a85236eec5b8035c54b1
edducon <e@panpushnyy.ru>
2024-09-29 16:12:15 +0300

    Метод вычисления гипотенузы + форматированный вывод

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Вычисление гипотенузы");
+        System.out.println("Введите первый катет:");
+        double a = in.nextDouble();
+        System.out.println("Введите второй катет:");
+        double b = in.nextDouble();
+        double result_hypotenuse = hypotenuse_calc(a, b);
+        System.out.printf("Длина гипотенузы (форм. вывод): %.2f%n", result_hypotenuse);
+
+    }
+
+    public static double hypotenuse_calc(double a, double b) {
+        return Math.sqrt(a * a + b * b);
     }
 }
+
