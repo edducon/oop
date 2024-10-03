52712bb0992eb5eab1bbb10d7f4f9ddf1cbb306c
edducon <e@panpushnyy.ru>
2024-10-03 16:23:54 +0300

    Перенос решений в методы

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,20 @@ import java.util.Scanner;
 
 public class Main {
 
+    public static double hypotenuse_calculate(double a, double b){
+        return Math.sqrt(a * a + b * b);
+    }
+
+    public static double calc_distance(double x1, double x2, double y1, double y2){
+        double deltaX = x2 - x1;
+        double deltaY = y2 - y1;
+        return Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+    }
+
+    public static int generateRandomInteger(int min, int max){
+        return (int) (Math.random() * (max - min + 1)) + min;
+    }
+
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
 
@@ -10,14 +24,11 @@ public class Main {
         System.out.println("Введите длину катета (b): ");
         double b = in.nextDouble();
 
-        double squareA = a * a;
-        double squareB = b * b;
-        double hypotenuse_calculate = Math.sqrt(squareA + squareB); //Метод вычисления длины гипотенузы
 
-        double result_1 = ((int)(hypotenuse_calculate * 100)) / 100.0; //Форматированный вывод
-        System.out.println("Длина гипотенузы (форм. вывод) = " + result_1);
-        double result_2 = (double) Math.round(hypotenuse_calculate * 100) / 100; //Метод Math.Round
-        System.out.println(" Длина гипотенузы (Math.round) = " + result_2);
+        double hypotenuse = hypotenuse_calculate(a, b);
+        System.out.printf("Длина гипотенузы (форматированный вывод): %.2f%n", hypotenuse);
+        double roundedHypotenuse = Math.round(hypotenuse * 100.0) / 100.0;
+        System.out.println("Длина гипотенузы (Math.round()): " + roundedHypotenuse);
 
 
 
@@ -30,14 +41,8 @@ public class Main {
         System.out.println("Введите координату y2");
         double y2 = in.nextDouble();
 
-        double deltaX = x2 - x1;
-        double deltaY = y2 - y1;
-        double distance = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
-
-        double distanceResult_1 = ((int)(distance * 100)) / 100.0;
-        System.out.println("Расстояние между двумя точками (форм. вывод) = " + distanceResult_1);
-        double distanceResult_2 = (double) Math.round(distance * 100) / 100;
-        System.out.println("Расстояние между двумя точками (Math.round) = " + distanceResult_2);
+        double distance = calc_distance(x1, x2, y1, y2);
+        System.out.printf("Расстояние между точками: %.2f%n", distance);
 
 
 
@@ -46,14 +51,7 @@ public class Main {
         System.out.println("Введите правую границу диапазона (целое число)");
         int max = in.nextInt();
 
-        if (min > max) {
-            int temp_number = min;
-            min = max;
-            max = temp_number;
-        }
-
-        int randomNum = (int) (Math.random() * (max - min + 1)) + min;
-        System.out.println("Случайное число из диапазона [" + min + ", " + max + "] = " + randomNum);
-
+        int randomNumber = generateRandomInteger(min, max);
+        System.out.println("Случайное число из диапазона [" + min + ", " + max + "]: " + randomNumber);
     }
 }
